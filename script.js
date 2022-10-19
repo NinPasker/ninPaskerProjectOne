732430ea6d686f1ae82169e4707339026f7f5fd3
Nin Pasker <gooddev.e.nin@gmail.com>
2022-10-19 17:51:38 -0400

    Added some comments to js file

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/* Message to the Reader/Reviewer
+
+This project has very little js functionality added to it
+
+Most of the hamburger menu was implemented through CSS/SaaS, only had to add a loop & eventListener to listen for a click on any of the links on the menu.
+
+Added some enabling/disabling features to the contact form which I learned from my side project which is linked below if you'd like to take a look at it (not optimized for mobile users... yet)
+
+Side Project - https://github.com/NinPasker/mapleM-Guild - I'd greatly appreciate it if you send some feedback about it if you have free time.
+
+I'll be launching it live soon to help with my recruitment process for a guild that I lead for a mobile-game😀 */
+
+
+
+/* Project One Related Javascript  */
+
 // targeting the links with the classes of mobile-menu
 const menu = document.querySelectorAll('.mobile-menu');
 // targeting the checkbox that opens the mobile menu
